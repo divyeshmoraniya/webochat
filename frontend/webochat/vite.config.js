81e88c51d78816ed6d56c2b46d1faf44e312c916
divyeshmoraniya <hdivyesh62@gmail.com>
2025-07-03 20:14:48 +0530

    fix

--- frontend/webochat/vite.config.js
+++ frontend/webochat/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173,
-    allowedHosts: ['https://webochat-2e2q.onrender.com/'], 
+    allowedHosts: ['webochat-2e2q.onrender.com'], 
   },
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173,
-    allowedHosts: ['https://webochat-2e2q.onrender.com/'], 
+    allowedHosts: ['webochat-2e2q.onrender.com'], 
   },
 });
