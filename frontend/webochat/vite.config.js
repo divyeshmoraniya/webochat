ebcb4a72a30e1cce74779e11da781746cc116d41
divyeshmoraniya <hdivyesh62@gmail.com>
2025-07-03 20:08:20 +0530

    fix

--- frontend/webochat/vite.config.js
+++ frontend/webochat/vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+    allowedHosts: ['webochat-pmeg.onrender.com'], 
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+    allowedHosts: ['webochat-pmeg.onrender.com'], 
+  },
+});
